d9fa533b74372ce5ae42fad396a9e8ee600d3fd0
aetherRohan <rohanahamed.cse103@gmail.com>
2026-01-21 00:27:15 +0530

    Initial commit

---
+++ src/main/java/com/attendifyserver/attendifyserver/AttendifyserverApplication.java
@@ -0,0 +1,13 @@
+package com.attendifyserver.attendifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendifyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendifyserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attendifyserver/attendifyserver/AttendifyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendifyserver.attendifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendifyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
