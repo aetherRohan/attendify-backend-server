fe2e443130ffa9f58e7385c4228ea5a9cef80206
aetherRohan <rohanahamed.cse103@gmail.com>
2026-02-24 02:04:38 +0530

    remove unnessecary line and space

--- src/main/java/com/attendifyserver/attendifyserver/controller/TeacherController.java
+++ src/main/java/com/attendifyserver/attendifyserver/controller/TeacherController.java
@@ -1,9 +1,6 @@
 package com.attendifyserver.attendifyserver.controller;
 
-import com.attendifyserver.attendifyserver.dto.CreateClassRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
